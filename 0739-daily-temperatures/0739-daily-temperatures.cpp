3aeb7007ce962796adcc6df56a034aef6b31a5ff
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2025-12-20 21:55:27 +0530

    Time: 15 ms (85.37%), Space: 102.9 MB (83.2%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector <int> answer(n, 0);
+
+        stack <int> st;
+
+        st.push(n-1);
+
+        for(int i = n-2; i>=0; i--){
+
+            while(!st.empty() && temperatures[st.top()] <= temperatures[i]){
+                st.pop();
+            }
+
+            if(!st.empty()){
+                answer[i] = st.top() - i;
+            }
+
+            st.push(i);
+
+        }
+        return answer;
+    }
+};
