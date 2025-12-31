3466ffb70f52edf9c809f064e35310c538314d30
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2025-12-31 20:18:17 +0530

    Time: 8 ms (10.31%), Space: 21.5 MB (7.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,42 +1,42 @@
-class Solution {
+// class Solution {
    
-public:
+// public:
 
-     void dfs(int node, vector<vector<int>>& adjList, vector<int>& visited){
-            visited[node] = 1;
-            for(auto nbr : adjList[node]){
-                if(!visited[nbr]){
-                    dfs(nbr, adjList, visited);
-                }
-            }
-        }
+//      void dfs(int node, vector<vector<int>>& adjList, vector<int>& visited){
+//             visited[node] = 1;
+//             for(auto nbr : adjList[node]){
+//                 if(!visited[nbr]){
+//                     dfs(nbr, adjList, visited);
+//                 }
+//             }
+//         }
 
 
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<int> visited(n, 0);
-        int count = 0;
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int n = isConnected.size();
+//         vector<int> visited(n, 0);
+//         int count = 0;
 
-        vector<vector<int>> adjList(n);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
-            }
-        }
+//         vector<vector<int>> adjList(n);
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 if(isConnected[i][j] == 1 && i != j){
+//                     adjList[i].push_back(j);
+//                     adjList[j].push_back(i);
+//                 }
+//             }
+//         }
 
-        for(int i=0; i<n; i++){
-            if(visited[i] == 0){
-                count++;
-                dfs(i, adjList, visited);
-            }
-        }
+//         for(int i=0; i<n; i++){
+//             if(visited[i] == 0){
+//                 count++;
+//                 dfs(i, adjList, visited);
+//             }
+//         }
 
-        return count;
-    }
-};
+//         return count;
+//     }
+// };
 
 // without making adjacency list
 
@@ -54,3 +54,50 @@ public:
         }
 
 */
+
+//BFS Method
+
+class Solution {
+   
+public:
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<vector<int>>adj(n);
+        vector<int> visited(n, 0);
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(isConnected[i][j]==1 && i != j){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+
+                }
+            }
+        }
+        int count = 0;
+
+        for(int i = 0; i<n; i++){
+            if(visited[i] == 0){
+                count++;
+                queue<int> q;
+                q.push(i);
+
+                visited[i] = 1;
+                while(!q.empty()){
+                    int f = q.front();
+                    q.pop();
+
+                    for(auto nbr : adj[f]){
+                        if(!visited[nbr]){
+                            q.push(nbr);
+                            visited[nbr] = 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
