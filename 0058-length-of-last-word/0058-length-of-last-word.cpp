e8ef94c68dcb9b8cc63b096ea8db5f088e68635a
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-03-06 00:18:57 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.81%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i = s.length() - 1;
+        int count = 0;
+
+        // skip trailing spaces
+        while(i >= 0 && s[i] == ' ')
+            i--;
+
+        // count last word characters
+        while(i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
+};
