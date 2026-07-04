d6627cd3f8727c133893c1136747f00eba02f06d
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-04 21:33:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (37.63%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,39 @@
+// class Solution {
+// public:
+//     int fib(int n) {
+//         vector<int> dp(n+1, -1);  // bottom up sc -> O(n);
+//         if(n <= 1){               // tc -> O(n);
+//             return n;
+//         }
+
+//         dp[0] = 0;
+//         dp[1] = 1;
+//         for(int i=2; i<n+1; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+
+//         return dp[n];
+//     }
+// };
+
+
 class Solution {
 public:
     int fib(int n) {
-        vector<int> dp(n+1, -1);
-        if(n <= 1){
+        //vector<int> dp(n+1, -1);  // bottom up sc -> O(n);
+        if(n <= 1){               // tc -> O(n);
             return n;
         }
 
-        dp[0] = 0;
-        dp[1] = 1;
-        for(int i=2; i<n+1; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        int a = 0;
+        int b = 1;
+        int c;
+        for(int i=1; i<n; i++){
+            c = a+b;
+            a = b;
+            b = c;
         }
 
-        return dp[n];
+        return c;
     }
 };
