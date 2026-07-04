ade092881addf3d23d2b3c96deb954f5753da4d5
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-04 10:39:48 +0530

    Time: 7 ms (1.81%), Space: 12.6 MB (6.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,18 +1,38 @@
+// class Solution {
+// public:
+//     bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int, int> freq_map;
+//         unordered_set<int> count_set;
+//         for(int i=0; i<arr.size(); i++){
+//             freq_map[arr[i]]++;
+//         }
+
+//         for(auto &it : freq_map){
+//             int freq = it.second;
+//             if(count_set.find(freq) != count_set.end()){
+//                 return false;
+//             }
+//             count_set.insert(freq);
+//         }
+
+//         return true;
+//     }
+// };
+
+
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> freq_map;
-        unordered_set<int> count_set;
-        for(int i=0; i<arr.size(); i++){
-            freq_map[arr[i]]++;
+        vector<int> vec(2001, 0);
+        for(int &x : arr){
+            vec[1000+x]++;
         }
 
-        for(auto &it : freq_map){
-            int freq = it.second;
-            if(count_set.find(freq) != count_set.end()){
+        sort(begin(vec), end(vec));
+        for(int i=1; i<2001; i++){
+            if(vec[i] != 0 && vec[i] == vec[i-1]){
                 return false;
             }
-            count_set.insert(freq);
         }
 
         return true;
