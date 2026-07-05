9f238a0343f9729d600bb9d68c3d8153506f8ae7
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-05 10:40:54 +0530

    Time: 0 ms (100%), Space: 9.9 MB (97.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,13 +3,14 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[1], nums[0]);
+        int a = nums[0];
+        int b = max(nums[1], nums[0]);
         for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1], nums[i]+dp[i-2]);
+            int c = max(b, nums[i] + a);
+            a = b;
+            b = c;
         }
 
-        return dp[n-1];
+        return b;
     }
 };
