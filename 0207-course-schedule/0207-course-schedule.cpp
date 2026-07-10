b31ecb2c8d7d5c88a752a46b5239c63eb7f0317e
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-10 21:59:30 +0530

    Time: 9 ms (26.9%), Space: 20.2 MB (25.02%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,40 +1,80 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
-        for(auto& x : prerequisites){
-            int u = x[0];
-            int v = x[1];
+        // vector<vector<int>> adj(numCourses);
+        // vector<int> indegree(numCourses, 0);
+        // for(auto& x : prerequisites){
+        //     int u = x[0];
+        //     int v = x[1];
 
-            adj[v].push_back(u);
+        //     adj[v].push_back(u);
+        // }
+        // for(int i=0; i<numCourses; i++){
+        //     for(int nbr : adj[i]){
+        //         indegree[nbr]++;
+        //     }
+        // }
+
+        // queue<int> q;
+        // for(int i=0; i<numCourses; i++){
+        //     if(indegree[i] == 0){
+        //         q.push(i);
+        //     }
+        // }
+
+        // vector<int> ans;
+
+        // while(!q.empty()){
+        //     int f = q.front();
+        //     q.pop();
+        //     ans.push_back(f);
+        //     for(int nbr : adj[f]){
+        //         indegree[nbr]--;
+        //         if(indegree[nbr] == 0){
+        //             q.push(nbr);
+        //         }
+        //     }
+        // }
+        // return ans.size() == numCourses;
+        int V = numCourses;
+        vector<vector<int>> adj(V);
+        for(auto it : prerequisites){
+            int u = it[1];
+            int v = it[0];
+            adj[u].push_back(v);
         }
-        for(int i=0; i<numCourses; i++){
-            for(int nbr : adj[i]){
+
+        vector<int> indegree(V,0);
+        for(int i=0; i<V; i++){
+            for(int &nbr : adj[i]){
                 indegree[nbr]++;
             }
         }
 
+        
+
         queue<int> q;
-        for(int i=0; i<numCourses; i++){
+        vector<int> topo;
+        for(int i=0; i<V; i++){
             if(indegree[i] == 0){
                 q.push(i);
             }
         }
-
-        vector<int> ans;
-
         while(!q.empty()){
             int f = q.front();
             q.pop();
-            ans.push_back(f);
-            for(int nbr : adj[f]){
+            topo.push_back(f);
+            for(auto &nbr : adj[f]){
+
                 indegree[nbr]--;
                 if(indegree[nbr] == 0){
                     q.push(nbr);
                 }
+                
             }
         }
-        return ans.size() == numCourses;
+
+        if(topo.size() == V) return true;
+        return false;
     }
 };
