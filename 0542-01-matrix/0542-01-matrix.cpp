95cbb67147c06683088f76a374342d015a8ffa40
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-11 15:14:53 +0530

    Time: 16 ms (66.68%), Space: 34.8 MB (66.66%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
-
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        vector<vector<int>> dist(n, vector<int>(m, 0));
-
-        queue<pair<pair<int, int>, int>> q;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i, j}, 0});
-                    visited[i][j] = 1;
+        int m = mat.size();
+        int n = mat[0].size(), dr[] = {0, 0, 1, -1}, dc[] = {1,-1,0,0};
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+        queue<pair<int, int>>q;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] == 0) {
+                    
+                    q.push({i, j});
                 }
-                else{
-                    visited[i][j] = 0;
-                }
+                else ans[i][j]=1e9;
             }
         }
-
-       
 
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int step = q.front().second;
+            int r = q.front().first, c = q.front().second;
             q.pop();
-            dist[r][c] = step;
-
-            vector<int> dx = {-1, 0, 1, 0};
-            vector<int> dy = {0, 1, 0, -1};
-
-            for(int i=0; i<4; i++){
-                int nrow = dx[i] + r;
-                int ncol = dy[i] + c;
-
-                if(nrow >=0 && ncol >= 0 && nrow < n && ncol < m && mat[nrow][ncol] == 1 && visited[nrow][ncol] == 0){
-                    q.push({{nrow, ncol}, step+1});
-                    visited[nrow][ncol] = 1;
+            for(int i=0;i<4;i++){
+                int nr = r + dr[i], nc = c + dc[i];
+                if(nr>=0 && nr<m && nc>= 0 && nc<n && mat[nr][nc] == 1){
+                    if(1+ans[r][c] < ans[nr][nc]) {
+                        ans[nr][nc] = 1 + ans[r][c];
+                        q.push({nr, nc});
+                    }
                 }
             }
         }
-
-        return dist;
-        
+        return ans;
     }
 };
