608d1613029e2004b6ca2d2ad919554b85687d7f
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-07-12 20:13:22 +0530

    Time: 1 ms (43.98%), Space: 13.4 MB (34.11%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> mp;
-        for(int & x : nums){
-            mp[x]++;
-        }
+        // map<int, int> mp;
+        // for(int & x : nums){
+        //     mp[x]++;
+        // }
 
-        for(auto &it : mp){
-            if(it.second == 1){
-                return it.first;
+        // for(auto &it : mp){
+        //     if(it.second == 1){           //brute force
+        //         return it.first;
+        //     }
+        // }
+
+        // return -1;
+
+        int result = 0;
+        for(int k=0; k<=31; k++){
+            int countOnes = 0;
+            int countZeroes = 0;
+            for(int &num : nums){
+                if((num & (1 << k)) != 0){
+                    countOnes++;
+                }
+                else{
+                    countZeroes++;
+                }
+            }
+
+            if(countOnes % 3 == 1){
+                result = result | (1 << k);
             }
         }
 
-        return -1;
+        return result;
     }
 };
