ad8a3a51450e3954c3fae34dd7a632bf3ac8b09b
Krishnam Singh <151383564+krishnam2003@users.noreply.github.com>
2026-08-11 01:15:06 +0530

    Time: 15 ms (18.2%), Space: 11.1 MB (19.52%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,26 +2,31 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
+
         unordered_map<char, int> mp;
+
         int maxFreq = 0;
         int maxLen = 0;
-
         int left = 0;
 
-        for(int right = 0; right < n; right++){
-            mp[s[right] - 'A']++;
+        for (int right = 0; right < n; right++) {
 
-            maxFreq = max(maxFreq, mp[s[right] - 'A']);
-            
-            while((right-left+1) - maxFreq > k){
-                mp[s[left] - 'A']--;
+            // Add current character
+            mp[s[right]]++;
+
+            // Maximum frequency in current window
+            maxFreq = max(maxFreq, mp[s[right]]);
+
+            // If replacements needed > k, shrink window
+            while ((right - left + 1) - maxFreq > k) {
+
+                mp[s[left]]--;
 
                 left++;
             }
 
-            if((right-left+1) - maxFreq <= k){
-                maxLen = max(maxLen, right-left+1);
-            }
+            // Update answer
+            maxLen = max(maxLen, right - left + 1);
         }
 
         return maxLen;
